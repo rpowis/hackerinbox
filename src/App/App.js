2354fc5fc0f2a26ae5cbd78bca1a70bacc7b5328
Rory Powis <rory.powis@digital.hmrc.gov.uk>
2017-05-19 09:22:25 +0200

    Create react app

---
+++ src/App/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Hacker News</h2>
+        </div>
+        <p className="App-intro">
+          Loading feed...
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
